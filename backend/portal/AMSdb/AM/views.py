e2b91c09eec916791acf1e093d7049ea690d00c4
TCON163 <84452309+TCON163@users.noreply.github.com>
2021-06-17 05:02:36 -0500

    Add files via upload

--- backend/portal/AMSdb/AM/views.py
+++ backend/portal/AMSdb/AM/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AppSerializer
+from .models import Applicant
 
 # Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = AppSerializer
+    queryset = Applicant.objects.all()
